a3bdf5e4784498c79c2b2871f434b84fc7a7367c
violethair <violethair2308@gmail.com>
2020-02-14 13:52:33 +0700

    Update install_docker_ce.sh

--- script/install_docker_ce.sh
+++ script/install_docker_ce.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce | echo Y
+echo yes | sudo apt install docker-ce
