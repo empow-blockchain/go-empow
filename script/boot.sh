16e6cdb7912bd28186164262de9684dadf3c68b8
Violet Hair <violethair2308@gmail.com>
2020-09-07 13:08:35 +0700

    Update boot.sh

--- script/boot.sh
+++ script/boot.sh
@@ -154,7 +154,7 @@ print_bye() {
 download_genesis_block() {
     echo 'Downloading Genesis Block ...'
 
-    hosts=("172.104.164.141")
+    hosts=("172.104.36.247")
     minTime=999999
     select_host=""
     for host in "${hosts[@]}"
